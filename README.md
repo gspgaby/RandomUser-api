5ee849cd1938f0936fbcf0e198a77a5387049379
gspgaby <gspgaby@gmail.com>
2021-03-28 16:04:38 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## RandomUser caseira:
+
+Entregável: uma API que retorna um usuário aleatório ao receber uma requisição GET no "/". As
+informações dos usuários devem conter, pelo menos:
+. Primeiro nome
+. Segundo nome
+. Gênero
+. Data de nascimento
+. Email
+. Senha
+
+## Technologies
+The following technologies was used to complete the challenge:
+
+. Node.js
+. Express
+. Faker
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require ('express');
+
+const faker = require ('faker');
+
+const app = express()
+
+const port = 3000;
+                                                                                         
+app.get('/', (req, res) => { res.send(
+    {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        address: {
+            city: faker.address.city(),
+            country: faker.address.country(),
+            phone: faker.phone.phoneNumber(),
+            state: faker.address.stateAbbr(),
+            street: faker.address.streetName(),
+            zipCode: faker.address.zipCode()
+        },
+        birthday: faker.date.past(),
+        email: faker.internet.email(),
+        userName: faker.internet.userName(),
+        password: faker.internet.password(),
+    }
+  );
+});
+
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`)
+});
